5f975d1abced9c94ba848605c82661e58175cc36
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-04-28 18:24:41 -0500

    styles: add external dep for water css

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
